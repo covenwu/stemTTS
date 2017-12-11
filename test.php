5cf096ae7b85ce757a51fc1c199685a1e6ba2eff
covenwu <jiwu8283>
2017-12-11 11:11:19 -0600

    hello world example

--- test.php
+++ test.php
@@ -1,7 +1,15 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: 20130027
  * Date: 2017/12/7
  * Time: 11:32
  */
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+ </body>
+</html>
