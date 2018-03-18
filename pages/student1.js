0f0a5f7cdb1442de833128c5283c6e14371fdec6
zhangrunquan <2041834343@qq.com>
2018-03-19 01:33:01 +0800

    2018.3.19学生页面提交作业实现
    简化了邮件列表，添加了提交作业功能

---
+++ pages/student1.js
@@ -0,0 +1,101 @@
+/*//1111部分为未完成部分，请忽略
+1.切换笔记本时清空输入框，添加”请输入作业内容“
+*/
+
+
+var student_id=1;           //1111 需要动态获取学生id的方法
+//headers数组设置邮件列表的表头
+var headers = ["邮件列表"];
+//初始时下拉栏状态记录为“主页”
+var listMood="主页"
+getData();
+
+//下拉菜单的选项被点击时listMood变量会改变为点击的按钮名（innerHTML),借此区分状态
+$(".listButton").click(function () {
+    changeListMood(this.innerHTML)
+});
+//登录按钮暂用于测试,此处应换为提交按钮
+$("#登录").click(function () {
+    if(listMood=="笔记本"){
+        submitHomework();
+    }
+});
+
+
+function getData(){
+    //ajax请求
+    $.get("student_get_email.php",{id:student_id},function(data){
+        //返回的json数据解码，数据存进data_array
+        var data_array=eval(data);
+        //eamil为显示邮件列表的div元素
+        var email=document.getElementById("email");
+        //创建表格
+        creatTable(email,headers,data_array);
+    })
+}
+
+
+//动态生成列表的函数
+function creatTable(parent, headers, datas) {
+
+    var table = document.createElement("table");
+    table.id = "tb";
+    parent.appendChild(table);
+
+    var thead = document.createElement("thead");
+    table.appendChild(thead);
+
+    var tr = document.createElement("tr");
+    thead.appendChild(tr);
+
+    /*for (var i = 0; i < headers.length; i++) {
+        var th = document.createElement("th");
+        th.innerHTML = headers[i];
+        tr.appendChild(th);
+    }*/
+
+    var tbody = document.createElement("tbody");
+    table.appendChild(tbody);
+
+    for (var i = 0; i < datas.length; i++) {
+        var tr = document.createElement("tr");
+        tbody.appendChild(tr);
+        //在新创建的行内填写除“操作”以外的列
+        /*for (var k in datas[i]) {
+            if(k!='emailcontent'){
+                var td = document.createElement("td");
+                td.innerHTML = datas[i][k];
+                tr.appendChild(td);
+            }
+        }*/
+
+        /*在新创建的行内填写“操作”列，附加点击展示邮件内容的功能*/
+        //设置新建的td元素
+        var display= document.createElement("td");
+        //display.innerHTML = "<a href='javascript:'></a>";
+        var emailnum=i+1;
+        display.innerHTML ="email"+emailnum;
+        tr.appendChild(display);
+        display.setAttribute('id','display'+i)
+        //取得emailcontent内容
+        var content=datas[i]['emailcontent'];
+        //设置点击展示邮件内容的功能
+        var disp=document.getElementById('display'+i);
+        disp.onclick = function () {
+            document.getElementById("emailcontent").value=content;
+        }
+    }
+}
+
+//切换笔记本和主页的函数,参数为代表状态的字符串
+function changeListMood(mood) {
+    listMood=mood;
+}
+
+function submitHomework() {
+    var text=document.getElementById("emailcontent").value;
+    $.get("submit_homework.php",{id:student_id,text:text},function(data){
+        //php文件运行成功返回的data为success
+        alert(data);
+    })
+}
--- pages/submit_homework.php
+++ pages/submit_homework.php
@@ -1,35 +1,39 @@
 <?php
 //学生提交作业的后台处理
-/*1.获取时间方法有问题
-2.插入语句调整
-3.获取taskid
+/*
+1.获取taskid
 */
 
 //-----------------连接mysql服务器----------------------------------------------
 $link =mysqli_connect('localhost:3306','root','12345678') ;
 //var_dump($link);
 $res=mysqli_set_charset($link,'utf8');
-
+//设置时区保证时间戳正确
+date_default_timezone_set('PRC');
 
 //选择数据库
 mysqli_query($link,'use database1');
 //将上传的聊天信息json格式化会导致乱码
-$msg=$_GET["msg"];
+$text=$_GET["text"];
 //获取id
 $id=$_GET['id'];
-//获取taskid
-$taskid=123;
-//获取time        此方法时区错误
+//获取taskid              //1111 应为动态获取
+$taskid=155;
+//获取time
 $time=date('Y-m-d H:i:s',time());
 
 
 //删除对应taskid旧记录
 $query="delete from homework_history where taskid='$taskid' AND id='$id' ";
 mysqli_query($link,$query);
-//插入对应taskid新纪录             //1111 应使用limit1提高性能，应调整格式增强可读性
+
+//-----------------对应插入新纪录----------------------------------------------
+//单独进行一些子查询以缩短insert指令，增强可读性
+$classid="(select(classid) from account where account.id='$id' limit 1)";
+$name="(select(name) from account where account.id='$id' limit 1)";
+
 $query="insert into homework_history(time,classid,id,name,taskid,homeworkcontent) values('$time'
-  ,(select(classid) from account where account.id='$id'),'$id',(select(name) from account where account.id='$id')
- ,'$taskid','$msg')";
+          ,$classid,'$id',$name,'$taskid','$text')";
 
 mysqli_query($link,$query);
 
